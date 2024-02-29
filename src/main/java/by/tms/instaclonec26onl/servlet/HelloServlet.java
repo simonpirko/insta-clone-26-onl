1bbf3c6327d0e3323f336678c3b6cc6967b7af4c
Simon Pirko <s.pirko@spotis.app>
2024-02-29 19:22:21 +0300

    Add Funcs

--- src/main/java/by/tms/instaclonec26onl/servlet/HelloServlet.java
+++ src/main/java/by/tms/instaclonec26onl/servlet/HelloServlet.java
@@ -15,6 +15,6 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+		System.out.println("Hello World!");
 	}
 }
