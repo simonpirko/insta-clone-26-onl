ccb0857267e82f2114e0bbbe76da30d48d07a6e9
Arthur <arthurdewitt6@gmail.com>
2024-03-01 21:43:03 +0300

    Implement Registration/authorization

--- src/main/java/by/tms/instaclonec26onl/admin/Admin.java
+++ src/main/java/by/tms/instaclonec26onl/admin/Admin.java
@@ -6,7 +6,6 @@ import by.tms.instaclonec26onl.storage.InMemoryUserStorage;
 // Создание админской учётки
 public class Admin extends User {
     private InMemoryUserStorage storage;
-
     public Admin(String username, String password, InMemoryUserStorage storage) {
         super(username, password);
         this.storage = storage;
