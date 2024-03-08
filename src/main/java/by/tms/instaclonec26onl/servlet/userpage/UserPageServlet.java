4eb72baa104d1b39ff884e71ca82b86d00963adc
Savva <sav.van.ivan3@mail.ru>
2024-03-08 21:40:21 +0300

    Search implementation

---
+++ src/main/java/by/tms/instaclonec26onl/servlet/userpage/UserPageServlet.java
@@ -0,0 +1,23 @@
+package by.tms.instaclonec26onl.servlet.userpage;
+
+import by.tms.instaclonec26onl.model.User;
+import by.tms.instaclonec26onl.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/userPage")
+public class UserPageServlet extends HttpServlet {
+    private final UserService userService = new UserService();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        User byUsername = userService.findByUsername(username);
+        req.setAttribute("username",byUsername);
+        getServletContext().getRequestDispatcher("/pages/user/user-page.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/webapp/pages/user/user-page.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Savva
+  Date: 08.03.2024
+  Time: 21:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>UserPage</title>
+</head>
+<body>
+<h1>${username.username}</h1>
+<form method="post" action="userPage">
+    <input type="hidden" name="username" value="${username.username}">
+    <button>sub</button>
+</form>
+</body>
+</html>
